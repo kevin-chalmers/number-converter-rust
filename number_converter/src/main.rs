3e09c991da2cc9df05960f6c6a8d06ed4c981f2f
Kevin Chalmers <drkevinchalmers@gmail.com>
2022-09-07 19:37:48 +0100

    Added check for number of arguments entered

--- number_converter/src/main.rs
+++ number_converter/src/main.rs
@@ -1,8 +1,17 @@
 use std::env;
+use std::process::exit;
 
 fn main() {
     // Collect the command line arguments
     let args: Vec<String> = env::args().collect();
+    // Check right number of arguments. Should have:
+    // -- the conversion type
+    // -- the input
+    if args.len() != 3 {
+        // Just print and exit for now
+        println!("Application takes two arguments");
+        exit(-1);
+    }
     // Iterate through the command line arguments
     for arg in args.iter() {
         println!("{}", arg);
