9b78103772f9dc648044c4c291aae857ae10c31f
Kevin Chalmers <drkevinchalmers@gmail.com>
2022-09-08 20:06:35 +0100

    Modified binary_to_digital to use Result

--- number_converter/src/main.rs
+++ number_converter/src/main.rs
@@ -2,24 +2,26 @@ use std::env;
 use std::process::exit;
 
 // Converts binary string to digital
-fn binary_to_digital(input: &str) {
+fn binary_to_digital(input: &str) -> Result<u128, &'static str> {
     // Check if input string too long
     if input.len() > 128 {
-        println!("Input string too long - 128 bits maximum");
-        return;
+        return Err("Input string too long - 128 bits maximum");
     }
     // Number created
     let mut num: u128 = 0;
     // Iterate through members of input string
     for c in input.chars() {
+        // Bitshift num -- increases power of two.
+        // At start has no effect -- 0 << 1 is still 0.
         num = num << 1;
+        // Check if character is 0, 1, or not allowed
         match c {
             '0' => (),
             '1' => num = num + 1,
-            _ => { println!("Input string can only contain 0 and 1"); exit(-1) }
-        }
+            _ => return Err("Input string can only contain 0 and 1")
+        };
     }
-    println!("{}", num);
+    Ok(num)
 }
 
 
@@ -37,7 +39,13 @@ fn main() {
     // Check second parameter for conversion type.
     // For now, just binary to digital.
     match args[1].as_str() {
-        "-b2d" => binary_to_digital(args[2].as_str()),
+        "-b2d" => { 
+            let result = binary_to_digital(args[2].as_str());
+            match result {
+                Ok(x) => println!("{}", x),
+                Err(e) => println!("{}", e)
+            };
+        },
         &_ => todo!()
     };
 }
